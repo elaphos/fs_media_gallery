0e4d9881bae251fd005e9f9c31ce44d1efb2d46a
Andreas Hirsch <ah@elaphos.de>
2026-06-29 16:19:32 +0200

    [RELEASE] 6.0.0: TYPO3 v14-Support (depends 14.4.99, branch-alias 6.0.x)

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ Show your media assets from your local or remote storage as a gallery of albums.
     'author_email' => 'franssaris@gmail.com',
     'author_company' => '',
     'state' => 'stable',
-    'version' => '5.0.0',
+    'version' => '6.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.4.99',
+            'typo3' => '13.0.0-14.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
